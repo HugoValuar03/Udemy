cb944d7e4455d3671004565953529f53007459c7
Hugo Valuar Bailona <142545466+HugoValuar03@users.noreply.github.com>
2023-10-12 17:47:19 -0300

    Add files via upload

--- lambdas/CalculoTeste3.java
+++ lambdas/CalculoTeste3.java
@@ -6,8 +6,15 @@ public class CalculoTeste3 {
 
 	public static void main(String[] args) {
 		
+		/* O BinaryOperator representa uma operação que recebe dois operandos do 
+		 * mesmo tipo e produz um resultado do mesmo tipo dos operandos
+		 */
 		BinaryOperator<Double> calc = (x, y) -> { return x + y; };
+		
 		System.out.println(calc.apply(2.0, 3.0));
+		/* O java não permite a conversão de int -> Double,
+		 * por isso é necessário colocar casas decimais
+		 */
 		
 		calc = (x, y) -> x*y;
 		
